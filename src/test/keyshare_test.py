4b92bc8be5db675dbf4c8d2c6a3af1e7489ce570
Christopher Wood <christopherwood07@gmail.com>
2013-01-15 01:28:10 -0500

    Outlined key share test

---
+++ src/test/keyshare_test.py
@@ -0,0 +1,5 @@
+import sys
+sys.path.append("../Logger/EncryptionModule.py")
+from EncryptionModule import EncryptionModule
+
+# TODO: create two separate encryption objects and attempt to share the key across them
