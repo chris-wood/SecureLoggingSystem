5fb81d99a6b6f8378b10c20d920fd695627b7188
Christopher Wood <christopherwood07@gmail.com>
2013-01-23 17:34:20 -0500

    Removed stupid files

--- src/v2/TestModule/json_test.py
+++
@@ -1,6 +0,0 @@
-import json
-
-msg = '{"command":"LOGIN","parameters":"caw,pw"}'
-data = json.loads(msg)
-print(data)
-print(data['command'])
--- src/v2/test/TestJSON.py
+++
@@ -1,4 +0,0 @@
-import json
-
-data = json.loads('{"user":0,"eventId":0,"payload":"TEST PAYLOAD"}')
-print data # missing an s... -_-
